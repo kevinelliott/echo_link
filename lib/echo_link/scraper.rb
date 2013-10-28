cad8398448220ffd5462787623e690562e1791fb
Kevin Elliott <kevin@welikeinc.com>
2013-10-28 11:10:48 -0700

    Make the EchoLink links scraper.

--- lib/echo_link/scraper.rb
+++ lib/echo_link/scraper.rb
@@ -4,85 +4,50 @@ require 'nokogiri'
 module EchoLink
   class Scraper
 
-    def reflectors
-      reflectors = []
 
-      html = html(Irlp::URLS[:reflectors])
-      html.css('table tr').each_with_index do |reflector, i|
-        next if i < 6
-        cols = reflector.css('td')
-        reflectors << {
-          id: cols[0].text.to_i,
-          name: cols[1].text.split.join(' ')
-        }
+    def links
+      links = []
+
+      first_page_url = "#{EchoLink::URLS[:all_links]}&offset=0"
+      html = html(first_page_url)
+      links += links_from_table(html.css('table')[5])
+
+      p = html.css('table').first.css('tr')[1].css('td').first.css('p').first
+      total_links = p.css('b')[2].text.to_i
+      per_page = 100
+      page_count = (total_links / per_page.to_f).ceil
+
+      (2..page_count).each do |num|
+        offset = per_page * (num - 1)
+        page_url = "#{EchoLink::URLS[:all_links]}&offset=#{offset}"
+
+        html = html(page_url)
+        links += links_from_table(html.css('table')[5])
       end
-
-      reflectors.each { |r| puts "#{r[:id]}: #{r[:name]}"}
-      reflectors
+      links
     end
 
-    def nodes
-      nodes = []
-
-      html = html(Irlp::URLS[:nodes])
-      html.css('table tr').each_with_index do |reflector, i|
-        next if i < 1
-        cols = reflector.css('td')
-        nodes << {
-          id: cols[0].text.to_i,
-          call_sign: cols[1].text,
-          city: cols[2].text,
-          province: cols[3].text,
-          country: cols[4].text,
-          frequency: cols[5].text,
-          status: cols[7].text.downcase,
-          status_duration: cols[8].text,
-          status_link: "#{Irlp::BASE_URL}/index.php?PSTART=11&nodeid=#{cols[0].text}"
+    def links_from_table(table)
+      links = []
+      table.css('tr').each_with_index do |tr, i|
+        next if i == 0
+        tds = tr.css('td')
+        links << {
+          id: tds[2].text.to_i,
+          call_sign: tds[0].text,
+          location: tds[3].text,
+          grid: tds[4].text,
+          frequency: tds[5].text,
+          tone_squelch: tds[6].text,
+          power: tds[7].text,
+          haat: tds[8].text,
+          antenna: tds[9].text,
+          status: tds[10].text,
+          comment: tds[11].text,
+          status_last_update_at: tds[12].text
         }
       end
-
-      nodes.each { |r| puts "#{r[:id]}: #{r[:call_sign]}"}
-      nodes
-    end
-
-    def node_status(id)
-      node = {}
-      
-      node_status_url = "#{Irlp::URLS[:node_detail]}&nodeid=#{id}"
-      html = html(node_status_url)
-      html.css('table tr').each_with_index do |row, i|
-        data = row.css('td')[1]
-        case i
-        when 0
-          node[:id] = data.text.to_i
-        when 1
-          node[:call_sign] = data.text
-        when 2
-          node[:city] = data.text
-        when 3
-          node[:province] = data.text
-        when 4
-          node[:country] = data.text
-        when 5
-          node[:owner] = data.text
-        when 6
-          node[:latitude] = data.text
-        when 7
-          node[:longitude] = data.text
-        when 8
-          node[:frequency] = data.text
-        when 9
-          node[:offset] = data.text
-        when 10
-          node[:tone_squelch] = data.text
-        when 11
-          node[:avrs_status] = data.text
-        when 12
-          node[:website] = data.css('a')[0].attr(:href).text if data.css('a').size > 0
-        end
-      end
-      
-      node
+      links
     end
 
     private
